06b68bc529c2ee2df6e4c0e838deb211d7d5de44
bbiscuit <ahuffman@cedarville.edu>
2022-03-08 11:24:08 -0700

    Simple styling for the list of links.

---
+++ js/index.js
@@ -0,0 +1,9 @@
+// Load links from document.
+
+var test1 = document.getElementById("a-test1");
+
+// Set button press event listeners.
+
+test1.addEventListener("click", function() {
+	console.log("does this work?");
+});
