1a088fad87dad0e354deed80afc612d1fe077783
bbiscuit <ahuffman@cedarville.edu>
2022-03-10 10:36:10 -0700

    Skeleton code for controls.

--- js/index.js
+++ js/index.js
@@ -30,3 +30,51 @@ var test1 = document.getElementById("a-test1");
 test1.addEventListener("click", function() {
 	console.log("does this work?");
 });
+
+// Set controls event listeners.
+
+// Load control elements.
+
+var playPause = document.getElementById("a-play-pause");
+var muteUnmute = document.getElementById("a-mute-unmute");
+var inputVolume = document.getElementById("input-volume");
+var volume = document.getElementById("p-volume");
+var inputSeekMinutes = document.getElementById("input-seek-minutes");
+var inputSeekSeconds = document.getElementById("input-seek-seconds");
+var seekLink = document.getElementById("a-seek");
+var seek = document.getElementById("p-seek");
+var duration = document.getElementById("p-duration");
+var paused = false;
+var muted = false;
+
+// Set control event listeners.
+
+playPause.addEventListener("click", function() {
+	paused = !paused;
+
+	if(paused) {
+		playPause.text = "Play";
+	}
+	else {
+		playPause.text = "Pause";
+	}
+});
+
+muteUnmute.addEventListener("click", function() {
+	muted = !muted;
+
+	if (muted) {
+		muteUnmute.text = "Unmute";
+	}
+	else {
+		muteUnmute.text = "Mute";
+	}
+});
+
+inputVolume.addEventListener("change", function() {
+	console.log("volume updated");
+});
+
+seekLink.addEventListener("click", function() {
+	console.log("seek initiated");
+});
